9fe2c81634fa7a371e81e72d5f4461addf72d4f4
krbriz <kristaps.brize@gmail.com>
2022-01-07 16:25:49 +0200

    a

--- labors_1_master.c
+++ labors_1_master.c
@@ -7,22 +7,30 @@ void main()
     double x, a0, S0;
     int k = 0, n, k1;
     printf("CosX apreekinaashana. Ievadi X: ");
+    printf(" \n");
     fflush(stdin);
     scanf("%lf", &x);
 
     printf("Summas izteiksme:            500            \n");
     printf("                             ____\n");
-    printf("                             \\        k    2*k     -2*k\n");
-    printf("                              \\    (-1) * x     * 2\n");
+    printf("                             \\        k    2*k     \n");
+    printf("                              \\    (-1) * x     \n");
     printf("                    cosx=       >   _______________________ \n");
     printf("                              // \n");
-    printf("                             //             (2*k!\n");
+    printf("                             //             2*k!\n");
     printf("                             k = 0\n");
-    printf("Definiicijas apgabals: X pieder R\n");
+    printf("\tDefiniicijas apgabals: X pieder R\n");
+    printf(" \n");
+    printf("Rekurences reizinaataajs:       1      2 \n");
+    printf("                             (-1)  *  x \n");
+    printf("                           _______________ \n");
+    printf("                                2k \n");
+    printf(" \n");
 
-    a0 = pow(-1, k) * pow(x, 2 * k) / (1.);
+    a0 = pow(-1, k) * pow(x, 2 * k) / (1);
     S0 = a0;
-    printf("a0 = %.2lf\t S0 = %.2lf\n", a0, S0);
+    printf(" \n");
+    printf("\ta0 = %.2lf\t S0 = %.2lf\n", a0, S0);
     fflush(stdin);
 
     a499 = a0;
@@ -35,14 +43,13 @@ void main()
             k1 = k;
         }
         k++;
-        a499 *= (-1) * x / ((2 * k) * (2 * k));
+        a499 *= (-1) * pow(x,2) / ((2 * k));
         S499 += a499;
-        printf("%d 499 = %Le\tS499 = %Le\n", k, a499, S499);
     }
     printf("a499 = %Le\tS499=%Le\n", a499, S499);
     printf("Peedeejaa nenulles veertiiba reekinot a499 ir a%d = %Le \n\n", k1, A499);
 
-    a500 = a499 * (-1) * x / ((2 * k) * (2 * k));
+    a500 = a499 * (-1) * pow(x,2) / (2 * k);
     S500 = S499 + a500;
     printf("a500 = %Le\tS500 = %Le\n", a500, S500);
 
