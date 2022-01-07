5c0efad47d597421b848ec886d41d606da4100d0
krbriz <kristaps.brize@gmail.com>
2022-01-07 14:18:41 +0200

    a

--- kaapinashanas_rezult.c
+++ kaapinashanas_rezult.c
@@ -7,19 +7,19 @@ int k=0;
 y=cos(x);
 printf("y=cos(%.2f)=%.2f\n",x,y);
 
-a=pow(-1,k)*pow(x,2*k)/(1);
+a=pow(-1,k)*pow(x,k)/(1);
 S=a;
 printf("%.2f\t%8.2f\t%8.2f\n",x,a,S);
 k++;
-a=a*(-1)*x*x/((2*k)*(2*k));//a=a∗(−1)∗x∗x/(2∗3);
+a=a*(-1)*x*x/((2*k)*(k));//a=a∗(−1)∗x∗x/(2∗3);
 S=S+a;
 printf("%.2f\t%8.2f\t%8.2f\n",x,a,S);
 k++;
-a=a*(-1)*x*x/((2*k)*(2*k));//a=a∗(−1)∗x∗x/(4∗5);
+a=a*(-1)*x*x/((2*k)*(k));//a=a∗(−1)∗x∗x/(4∗5);
 S=S+a;
 printf("%.2f\t%8.2f\t%8.2f\n",x,a,S);
 k++;
-a=a*(-1)*x*x/((2*k)*(2*k));//a=a∗(−1)∗x∗x/(6∗7);
+a=a*(-1)*x*x/((2*k)*(k));//a=a∗(−1)∗x∗x/(6∗7);
 S=S+a;
 printf("%.2f\t%8.2f\t%8.2f\n",x,a,S);
 }
